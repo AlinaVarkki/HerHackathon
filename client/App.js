3c5344802802911dfe7964dd40bc83ba906ccb83
Slavka Borovska <gwb18150@strath.ac.uk>
2021-06-24 20:43:06 +0100

    commit attempt

--- client/App.js
+++ client/App.js
@@ -23,7 +23,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your awtwwp!</Text>
+      <Text>Open up App.js to start working on your awtwwp hello!</Text>
         <Text>{state.myText}</Text>
       <Button
           onPress={onPressLearnMore}
