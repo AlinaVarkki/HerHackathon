1d6ebdee50200297b3cf23cde2411863b0a0bfb3
Slavka Borovska <gwb18150@strath.ac.uk>
2021-06-25 19:28:29 +0100

    sorry, forgot to delete one thing

--- client/views/TeamScreen.js
+++ client/views/TeamScreen.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import {StyleSheet, View, Text} from "react-native";
 import ColorPalette from "../Assets/ColorPalette";
-import HorizontalMenuV2 from "../Components/HorizontalMenuV2";
 
 const TeamScreen = () => {
 
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Team name</Text>
-            <HorizontalMenuV2/>
             <Text></Text>
         </View>
     );
