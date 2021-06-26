2074b607baf03c14d9ef6c7d63afde3ff27b0b8b
Slavka Borovska <gwb18150@strath.ac.uk>
2021-06-26 23:06:28 +0100

    put final data in leaderboard

--- client/Components/RestLeaderboard.js
+++ client/Components/RestLeaderboard.js
@@ -6,12 +6,25 @@ import RectangularLeaderboard from "./RectangularLeaderboard";
 
 
 const RestLeaderboard = () => {
+    const teams = [
+        {
+            name: "Club Penguin",
+            position: "4",
+            photo: require('../Assets/Teams/Artboard17-1.png'),
+            points: "1001",
+        },{
+            name: "Cool boiz",
+            position: "5",
+            photo: require('../Assets/Teams/Artboard20-1.png'),
+            points: "999",
+        }
+    ]
 
     return (
         <View>
-            <RectangularLeaderboard title='4' teamName='The Straths' photoURL={require('../Assets/tempProfilePic.png')} points='200'/>
-            <RectangularLeaderboard title='5' teamName='The Straths' photoURL={require('../Assets/tempProfilePic.png')} points='200'/>
-            <RectangularLeaderboard title='6' teamName='The Straths' photoURL={require('../Assets/tempProfilePic.png')} points='200'/>
+            <RectangularLeaderboard title={teams[0].position} photoURL={teams[0].photo} teamName={teams[0].name} points={teams[0].points} color={ColorPalette.green}/>
+
+            <RectangularLeaderboard title={teams[1].position} photoURL={teams[1].photo} teamName={teams[1].name} points={teams[1].points} color={ColorPalette.orange}/>
 
 
         </View>
