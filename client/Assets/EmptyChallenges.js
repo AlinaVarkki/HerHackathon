d24c8491091db41daeaf26bd54d753ac3d15b405
Slavka Borovska <gwb18150@strath.ac.uk>
2021-06-26 17:01:49 +0100

    committing images

---
+++ client/Assets/EmptyChallenges.js
@@ -0,0 +1,34 @@
+export default class ActivityList {
+
+
+
+    static activities = [
+
+        {
+            id: '1',
+            title: 'Empty challenge',
+            description: 'Lorem Ipsum... I do not know what to put here lol lmao',
+            category: 'FITNESS',
+            image: require('../Assets/ChallangeImages/Artboard 1.png')
+        },
+        {
+            id: '2',
+            title: 'Empty challenge',
+            description: 'Lorem Ipsum... I do not know what to put here lol lmao',
+            category: 'FITNESS',
+            image: require('../Assets/ChallangeImages/Artboard 2.png')
+        },
+        {
+            id: '3',
+            title: 'Empty challenge',
+            description: 'Lorem Ipsum... I do not know what to put here lol lmao',
+            category: 'FITNESS',
+            image: require('../Assets/ChallangeImages/Artboard 3.png')
+        }
+
+
+
+    ]
+
+
+}
